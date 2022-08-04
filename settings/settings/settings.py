f9d847017651243e3e13b442db79a39b6d6d93ca
Ravi ARS <ravi8ars28@gmail.com>
2022-08-04 07:47:42 +0530

    added missed background imgs

--- settings/settings/settings.py
+++ settings/settings/settings.py
@@ -112,8 +112,6 @@ USE_TZ = True
 
 STATIC_URL = 'static/'
 
-# Default primary key field type
-# https://docs.djangoproject.com/en/4.0/ref/settings/#default-auto-field
 STATICFILES_DIRS = [
     os.path.join(BASE_DIR, "static"),
     '/static/',
@@ -122,4 +120,6 @@ STATICFILES_DIRS = [
 MEDIA_ROOT = os.path.join(BASE_DIR, 'media')
 MEDIA_URL = '/media/'
 
+# Default primary key field type
+# https://docs.djangoproject.com/en/4.0/ref/settings/#default-auto-field
 DEFAULT_AUTO_FIELD = 'django.db.models.BigAutoField'
